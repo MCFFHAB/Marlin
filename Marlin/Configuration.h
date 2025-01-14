8c536cf33e3e380248ce527782df8f6e4702a60b
MCFFHAB <abnet@abreu.net>
2025-01-14 11:36:10 -0600

    Update 2

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "E5P-OPV1.1-V1"
+#define CUSTOM_MACHINE_NAME "E5P-OPV1.1-V3"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
